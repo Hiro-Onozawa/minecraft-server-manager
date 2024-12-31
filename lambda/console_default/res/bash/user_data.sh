be5f88e94084a4301f59c2de53c6ecd59f129271
Hiro-Onozawa <48467312+Hiro-Onozawa@users.noreply.github.com>
2024-12-31 22:25:39 +0900

    git cloneをsshからhttpsへ変更

--- lambda/console_default/res/bash/user_data.sh
+++ lambda/console_default/res/bash/user_data.sh
@@ -7,5 +7,5 @@ unzip -q awscliv2.zip
 rm -rf ./awscliv2.zip ./aws
 sudo -u ubuntu mkdir -p /home/ubuntu/workspace
 cd /home/ubuntu/workspace
-sudo -u ubuntu git clone git@github.com:Hiro-Onozawa/minecraft-server-manager.git user_util
+sudo -u ubuntu git clone https://github.com/Hiro-Onozawa/minecraft-server-manager.git user_util
 sudo -u ubuntu bash /home/ubuntu/workspace/user_util/bash/initialize.sh %%ON_MOUNT_ARG%%
