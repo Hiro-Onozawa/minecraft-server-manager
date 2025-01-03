ac606e9c81992e7f9a027b14a30f942eb04e72b3
Hiro-Onozawa <48467312+Hiro-Onozawa@users.noreply.github.com>
2025-01-03 21:58:12 +0900

    サーバー状態確認処理のデバッグログ追加

--- bash/monitor/server_task.sh
+++ bash/monitor/server_task.sh
@@ -10,6 +10,8 @@ set +e
 while true
 do
   monitor/server_core.sh
-  echo "$? : $(TZ=JST-9 date) - $(find "${SERVER_HOME}" -name "server_*" -printf "%f,")"
+  LAST_STATUS_CODE=$?
+  source monitor/last_state_get.sh
+  echo "${LAST_STATUS_CODE} : $(TZ=JST-9 date) - $(find "${SERVER_HOME}" -name "server_*" -printf "%f,") - ${SERVER_LAST_STATE}"
   sleep 20
 done
